0a8cc7ad19c316397c70c3591976b1bb539807d5
Abhi Nayak <anayak@medline.com>
2024-07-11 11:49:08 -0500

    Added Python file that includes email sorting Algorithm

---
+++ sortEmail.py
@@ -0,0 +1,9 @@
+'''
+function name: sortEmail
+inputs: email - Email Object to be sorted
+outputs: None
+side effects: Pushes email object into a database where it will be sorted by Priority Level and Subject Line
+References:
+'''
+def sortEmail(email):
+    
